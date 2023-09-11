1b8186a872d365cf944227c25d5f9b8d004d695f
Adeniji Ridwan Opeyemi <111622920+adenijiridwan43@users.noreply.github.com>
2023-09-10 20:29:31 -0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h@m4(34le6^n#h!q-$5@70$cpfko6g@l9va2e$k)4ean-oq_-%'
+SECRET_KEY = '🎉'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
